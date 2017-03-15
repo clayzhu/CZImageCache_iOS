51e0316b95eb267e00cca012ff7ec74b18749260
Clay Zhu <clayzhu@163.com>
2017-03-16 00:18:48 +0800

    创建项目。

---
+++ CZImageCache/CZImageCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZImageCache
+//
+//  Created by ug19 on 2017/3/16.
+//  Copyright © 2017年 Ugood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
