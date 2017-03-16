e5eeace61251f6be00668a74bd9ff51a62b137c8
Clay Zhu <clayzhu@163.com>
2017-03-16 16:13:21 +0800

    封装对图片请求、缓存、清理缓存等操作。

--- CZImageCacheDemo/CZImageCache/CZImageCache.h
+++ CZImageCacheDemo/CZImageCache/CZImageCache.h
@@ -7,10 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface CZImageCache : NSObject
 
 /** 图片缓存类的单例方法 */
 + (instancetype)sharedInstance;
+
+#pragma mark - 计算、清理缓存
+/** 计算所有缓存的大小，单位为 M */
+- (CGFloat)cacheSize;
+/** 计算图片缓存的大小，单位为 M */
+- (CGFloat)imageCacheSize;
+
+/** 删除所有缓存 */
+- (void)clearCacheWithCompletion:(void (^)(void))completion;
+/** 删除图片缓存 */
+- (void)clearImageCacheWithCompletion:(void (^)(void))completion;
+
+#pragma mark - 对图片缓存
+/**
+ *  根据图片 URL 缓存图片到沙盒 Caches/ImageCache 目录中。通过图片 URL 缓存
+ *
+ *  @param image   需要缓存的图片
+ *  @param url     图片 URL
+ *
+ *  @return YES 缓存成功，NO 缓存失败
+ */
+- (BOOL)saveImage:(UIImage *)image imageURL:(NSString *)url;
+
+/**
+ *  根据图片 URL 为一个 imageView 设置图片，缓存中存在该图片直接读取缓存，否则通过网络下载。可以设置一个默认图片，success 和 failure 在网络下载的情况下调用。通过图片 URL 缓存
+ *
+ *  @param imageView        显示图片的 imageView
+ *  @param url              图片 URL
+ *  @param placeholderImage 默认图片
+ *  @param success          缓存中无图片，通过网络下载图片成功后的 block
+ */
+- (void)imageForImageView:(UIImageView *)imageView
+				 imageURL:(NSString *)url
+		 placeholderImage:(UIImage *)placeholderImage
+				  success:(void (^)(UIImage *image))success;
 
 @end
--- CZImageCacheDemo/CZImageCache/CZImageCache.m
+++ CZImageCacheDemo/CZImageCache/CZImageCache.m
@@ -11,6 +11,7 @@
 @interface CZImageCache ()
 
 @property (strong, nonatomic) NSString *imageCachePath;
+@property (strong, nonatomic) NSString *cachePath;
 
 @end
 
@@ -34,14 +35,134 @@
 
 - (NSString *)prepareImagePath {
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-	NSString *documentsDirectory = [paths objectAtIndex:0];
+	NSString *cachesDirectory = [paths objectAtIndex:0];
+	self.cachePath = cachesDirectory;
 	// 图片缓存到沙盒 Caches 的 CachesImage 目录中
-	NSString *dataPath = [documentsDirectory stringByAppendingPathComponent:@"ImageCache"];
+	NSString *dataPath = [cachesDirectory stringByAppendingPathComponent:@"ImageCache"];
 	NSError *error;
 	BOOL isDirectory = YES;
 	if (![[NSFileManager defaultManager] fileExistsAtPath:dataPath isDirectory:&isDirectory])
 		[[NSFileManager defaultManager] createDirectoryAtPath:dataPath withIntermediateDirectories:NO attributes:nil error:&error];
 	return dataPath;
+}
+
+#pragma mark - 计算、清理缓存
+- (long long)fileSizeAtPath:(NSString *)filePath {
+	NSFileManager *manager = [NSFileManager defaultManager];
+	if ([manager fileExistsAtPath:filePath]) {
+		return [[manager attributesOfItemAtPath:filePath error:nil] fileSize];
+	}
+	return 0;
+}
+
+- (CGFloat)cacheSize {
+	NSFileManager *manager = [NSFileManager defaultManager];
+	if (![manager fileExistsAtPath:self.cachePath]) return 0.0;
+	NSEnumerator *childFilesEnumerator = [[manager subpathsAtPath:self.cachePath] objectEnumerator];
+	NSString *fileName;
+	long long folderSize = 0;
+	while ((fileName = [childFilesEnumerator nextObject]) != nil) {
+		NSString *fileAbsolutePath = [self.cachePath stringByAppendingPathComponent:fileName];
+		folderSize += [self fileSizeAtPath:fileAbsolutePath];
+	}
+	return folderSize / (1024.0 * 1024.0);
+}
+
+- (CGFloat)imageCacheSize {
+	NSFileManager *manager = [NSFileManager defaultManager];
+	if (![manager fileExistsAtPath:self.imageCachePath]) return 0.0;
+	NSEnumerator *childFilesEnumerator = [[manager subpathsAtPath:self.imageCachePath] objectEnumerator];
+	NSString *fileName;
+	long long folderSize = 0;
+	while ((fileName = [childFilesEnumerator nextObject]) != nil) {
+		NSString *fileAbsolutePath = [self.imageCachePath stringByAppendingPathComponent:fileName];
+		folderSize += [self fileSizeAtPath:fileAbsolutePath];
+	}
+	return folderSize / (1024.0 * 1024.0);
+}
+
+- (void)clearCacheWithCompletion:(void (^)(void))completion {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSArray *files = [[NSFileManager defaultManager] subpathsAtPath:self.cachePath];
+		for (NSString *p in files) {
+			NSError *error;
+			NSString *path = [self.cachePath stringByAppendingPathComponent:p];
+			if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+				[[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+			}
+		}
+		dispatch_sync(dispatch_get_main_queue(), ^{
+			if (completion) {
+				completion();
+			}
+		});
+	});
+}
+
+- (void)clearImageCacheWithCompletion:(void (^)(void))completion {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSArray *files = [[NSFileManager defaultManager] subpathsAtPath:self.imageCachePath];
+		for (NSString *p in files) {
+			NSError *error;
+			NSString *path = [self.imageCachePath stringByAppendingPathComponent:p];
+			if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+				[[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+			}
+		}
+		dispatch_sync(dispatch_get_main_queue(), ^{
+			if (completion) {
+				completion();
+			}
+		});
+	});
+}
+
+#pragma mark - 对图片缓存
+- (BOOL)saveImage:(UIImage *)image imageURL:(NSString *)url {
+	NSString *imageTitle = [url stringByReplacingOccurrencesOfString:@"/" withString:@"%2f"];
+	NSString *imagePath = [self.imageCachePath stringByAppendingPathComponent:imageTitle];
+//	NSData *imageData = UIImagePNGRepresentation(image);
+	NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+	BOOL writeOK = [imageData writeToFile:imagePath atomically:YES];
+	return writeOK;
+}
+
+- (void)imageForImageView:(UIImageView *)imageView
+				 imageURL:(NSString *)url
+		 placeholderImage:(UIImage *)placeholderImage
+				  success:(void (^)(UIImage *image))success {
+	// 先去沙盒里去寻找是否有这个 URL 对应的图片
+	NSString *imageTitle = [url stringByReplacingOccurrencesOfString:@"/" withString:@"%2f"];
+	NSString *imagePath = [self.imageCachePath stringByAppendingPathComponent:imageTitle];
+	UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+	if (image) {	// 缓存中有该图片
+		imageView.image = image;
+		if (success) {
+			success(image);
+		}
+	} else {	// 缓存中无该图片，需要网络请求
+		// 设置了默认图片则显示默认图片
+		imageView.image = placeholderImage;
+		// 图片在网络下载时显示一个菊花
+		UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+		[imageView addSubview:indicator];
+		indicator.color = [UIColor grayColor];
+		indicator.center = CGPointMake(CGRectGetWidth(imageView.bounds) / 2, CGRectGetHeight(imageView.bounds) / 2);
+		[indicator startAnimating];
+		
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:[url stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]]];
+			dispatch_sync(dispatch_get_main_queue(), ^{
+				[data writeToFile:imagePath atomically:YES];
+				UIImage *image = [UIImage imageWithData:data];
+				imageView.image = image;
+				[indicator stopAnimating];
+				if (success) {
+					success(image);
+				}
+			});
+		});
+	}
 }
 
 @end
