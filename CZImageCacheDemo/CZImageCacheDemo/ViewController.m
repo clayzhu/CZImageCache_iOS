4c66b463f2bcf54f955d9b4065b3e4e92e4142e1
Clay Zhu <clayzhu@163.com>
2017-03-16 16:13:30 +0800

    Demo.

--- CZImageCacheDemo/CZImageCacheDemo/ViewController.m
+++ CZImageCacheDemo/CZImageCacheDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "CZImageCache.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *allCacheSizeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *imageCacheSizeLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,6 +21,13 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	[self setupCacheLabel];
+	[[CZImageCache sharedInstance] imageForImageView:self.imageView
+											imageURL:@"http://ompmj0bxx.bkt.clouddn.com/IMG_1382.JPG"
+									placeholderImage:nil
+											 success:^(UIImage *image) {
+												 [self setupCacheLabel];
+											 }];
 }
 
 
@@ -25,5 +36,21 @@
 	// Dispose of any resources that can be recreated.
 }
 
+- (void)setupCacheLabel {
+	self.allCacheSizeLbl.text = [NSString stringWithFormat:@"全部缓存大小：%.2fM", [[CZImageCache sharedInstance] cacheSize]];
+	self.imageCacheSizeLbl.text = [NSString stringWithFormat:@"图片缓存大小：%.2fM", [[CZImageCache sharedInstance] imageCacheSize]];
+}
+
+- (IBAction)clearAllCacheAction:(UIButton *)sender {
+	[[CZImageCache sharedInstance] clearCacheWithCompletion:^{
+		[self setupCacheLabel];
+	}];
+}
+
+- (IBAction)clearImageCacheAction:(UIButton *)sender {
+	[[CZImageCache sharedInstance] clearImageCacheWithCompletion:^{
+		[self setupCacheLabel];
+	}];
+}
 
 @end
