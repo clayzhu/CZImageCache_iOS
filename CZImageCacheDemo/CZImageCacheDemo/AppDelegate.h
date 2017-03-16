310484e756a415144ef4848336f6c625175b6834
Clay Zhu <clayzhu@163.com>
2017-03-16 14:08:05 +0800

    重建项目。

---
+++ CZImageCacheDemo/CZImageCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZImageCacheDemo
+//
+//  Created by Apple on 2017/3/16.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZImageCacheDemo/CZImageCacheDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZImageCacheDemo
+//
+//  Created by Apple on 2017/3/16.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
